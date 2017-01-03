f2bf98df54d173691b7cb157ae290a9ca91ee7b7
An Khong <andkhong@gmail.com>
2017-01-03 00:25:43 -0800

    added config feature

--- line-count.js
+++ line-count.js
@@ -1,37 +1,40 @@
 const fs = require('fs');
 const path = require('path');
-const config = require('./line-count.config.js');
+
+let config = require('./default.config.js');
 
 // resolves with the file names within the given directory
-function get_file_names(dir) {
+function getFileNames(dir){
   return new Promise((resolve, reject) => {
     fs.readdir(dir, (err, fileNames) => {
       if (err) return reject(err);
       resolve(fileNames);
     });
   });
-}
+};
 
 // resolves with an object containing the type ('file' or 'dir') for the given file path and the file path itself: { file_path, type }
-function get_path_and_type(file_path) {
+function getPathAndType(filePath){
   return new Promise((resolve, reject) => {
-    fs.stat(file_path, (err, stat) => {
+    fs.stat(filePath, (err, stat) => {
       if (err) return reject(err);
       if (!stat.isDirectory() && !stat.isFile()) reject('Invalid Type');
+      // If directory has config file labled line.config.js, reassign config
+      if(filePath.endsWith('line.config.js')) config = require(path.resolve('..', filePath));      
       const type = stat.isDirectory() ? 'dir' : 'file';
       resolve({
-        file_path,
+        filePath,
         type
       });
     });
   });
-}
+};
 
 // same as before, counts lines for the given file path
-function count_lines(file_path) {
+function countFileLines(filePath){
   return new Promise((resolve, reject) => {
     let lineCount = 0;
-    fs.createReadStream(file_path)
+    fs.createReadStream(filePath)
       .on("data", (buffer) => {
         buffer.forEach((chunk) => {
           if (chunk === 10) lineCount++;
@@ -42,7 +45,7 @@ function count_lines(file_path) {
   });
 };
 
-function get_dir_line_count(dir) {
+function getDirLineCount(dir){
 
   const output = {
     file_count: 0,
@@ -51,59 +54,63 @@ function get_dir_line_count(dir) {
   };
 
   // get all filenames in the given directory
-  return get_file_names(dir)
-    // filter all file names that start with a '.' or include the string 'node_modules'
+  return getFileNames(dir)
+    // filter out hidden files/directory
     .then((names) =>
       names.filter((name) =>
-        !name.startsWith('.') && !name.includes(config.exclude[0])
+        !name.startsWith('.')
       )
-    )
+    ).catch(console.error)
     // map every file name into a promise that resolves with the type for that file name within the given dir
     .then((names) =>
       names.map((name) =>
-        get_path_and_type(path.join(dir, name))
-        .catch(console.warn) // log invalid typed files if necessary
+        getPathAndType(path.join(dir, name))
+          .catch(console.warn) // log invalid typed files if necessary
       )
-    ).then((paths_and_types_promises) =>
-      Promise.all(paths_and_types_promises.map((promise) =>
+    ).then((pathsAndTypesPromises) =>
+      Promise.all(pathsAndTypesPromises.map((promise) =>
         promise.then(({
-          file_path,
+          filePath,
           type
         }) => {
           if (type === 'dir') {
             // if current file path corresponds to a directory
             // recursive count its files and lines and add it to the overall output
-            return get_dir_line_count(file_path)
+            for(let i = 0; i < config.exclude.length; i++){
+              if(filePath.endsWith(config.exclude[i])) return;
+            };
+            return getDirLineCount(filePath)
               .then((recursive_output) => {
-                if(recursive_output){
+                // if(recursive_output){
                   output.file_count += recursive_output.file_count;
                   output.file_lines += recursive_output.file_lines;
-                }
-              })
+                // }
+
+              }).catch(console.error);
           } else {
             // count the lines for the current file path and then update the overall output
-            /// Modularize this section
-            let file_path_ext = path.extname(file_path);
-            for(var i = 0; i < config.extensions.length; i++){
-              if(file_path_ext === config.extensions[i]){
-                return count_lines(file_path)
-                  .then((file_lines) => {
-                    output.file_lines += file_lines;
+            let filePathExt = path.extname(filePath);
+            for(let i = 0; i < config.extensions.length; i++){
+              if(filePathExt === config.extensions[i]){
+                return countFileLines(filePath)
+                  .then((fileLines) => {
+                    output.file_lines += fileLines;
                     output.file_count += 1;
-                  })
-              }
-            }
-            ////
-          }
+                  }).catch(console.error);
+              };
+            };
+          };
         })
       ))
     // this last chain makes sure we wait for the promise to resolve
     // and populate the output object before resolving with it
   ).then( () => {
-    console.log(output.path, '=', output.file_count, 'files,', output.file_lines, 'lines');
+    // Split & Pop performs better than Regex & is easier to maintain
+    console.log('\t', output.path.split('/').pop() + '/', '=', output.file_count, 'files,', output.file_lines, 'lines');
     return output;
-  })
-}
+  }).catch(console.error);
+};
 
-const directory = process.argv[2];
-get_dir_line_count('./../' + directory)
+const directory = path.resolve('..', process.argv[2]);
+getDirLineCount(directory)
+  .catch(console.error)
