48cc1889b4fa58699c5b61a9f67141b28ab29e6b
An Khong <andkhong@gmail.com>
2017-01-29 00:29:20 -0800

    fixed path length bug if certain paths are too small

--- index.js
+++ index.js
@@ -6,30 +6,33 @@ const argv = require('minimist')(process.argv.slice(2));
 if(argv._.length <= 0){
   lineCount.getDirLineCount(process.env.PWD)
     .then( (result) =>
-      displayTreeBFS(result)
+      displayTreeDFS(result, print)
     ).catch(console.error);
 } else {
   for(let i = 0; i < argv._.length; i++){
     let directory = path.resolve('', argv._[i]);
     lineCount.getDirLineCount(directory)
       .then( (result) =>
-        displayTreeBFS(result)
+        displayTreeDFS(result, print)
       ).catch(console.error);
   }
 }
 
-const displayTreeBFS = (tree) => {
+const displayTreeDFS = (tree, callback) => {
   let stack = [tree];
   while(stack.length > 0){
     let item = stack.pop();
-
-    let data = item.path.split('/');
-    console.log('\t'.repeat(data.length-5), data.pop() + '/', item.file_count, 'files,', item.file_lines, 'lines');
-
+    callback(item);
     if (item.children){
       for(let i = 0; i < item.children.length; i++){
         stack.push(item.children[i]);
       }
     } else continue;
   }
+}
+
+const print = (object) => {
+  const data = object.path.split('/');
+  if(data.length < 5) return;
+  else console.log('\t'.repeat(data.length-5), data.pop() + '/', object.file_count, 'files,', object.file_lines, 'lines');
 }
